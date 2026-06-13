a758e55eec227557b8a0ac8da806e3f6a52a8224
samwasted <samarpancodes@gmail.com>
2026-06-13 20:06:05 +0530

    cloudflare fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  adapter: cloudflare({
+    imageService: 'passthrough',
+  }),
   site: 'https://samarpanverma.com',
   prefetch: {
     prefetchAll: true,
